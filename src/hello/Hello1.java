f07415db5e51f66a78ebe2fe32658d722cd6ccfc
ilir ahmeti <ilirahmeti.com>
2018-09-10 12:24:29 -0400

    added Hello World Program

---
+++ src/hello/Hello1.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Hello1 {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=10; i++){
+            System.out.println("Hello world ... " + i);
+        }
+    }
+}
